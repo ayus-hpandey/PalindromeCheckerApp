efc24b27ba48c586611ec3fe070735673c92a571
ayus-hpandey <pandeyayush0990@gmail.com>
2026-02-27 14:40:21 +0530

    Welcome Message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * MAIN CLASS - UserCase1Palindrome APP
+ *
+ * USE CASE 1: Application Entry and Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Palindrome Checker Management System.
+ * It displays a welcome message
+ * Shows application version
+ *
+ * @author Ayush
+ * @version 1.0
+ */
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
+
+    }
+}
